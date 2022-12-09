6447f438c870094b72f4c03fa9752d0e608e87be
Blockcheers <62717627+talhajaami@users.noreply.github.com>
2022-12-09 23:05:47 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route
-                path='/token'
+                path='/'
                 element={<Mint />}
               />
             </Routes>
